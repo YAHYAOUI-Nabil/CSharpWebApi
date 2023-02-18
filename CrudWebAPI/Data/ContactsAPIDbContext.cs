99514b7eea2df38856f63cde0be0fcd06585384e
Yahyaoui Nabil <yahyaouinabil1993@gmail.com>
2023-02-18 17:27:41 +0100

    Ajoutez des fichiers projet.

---
+++ CrudWebAPI/Data/ContactsAPIDbContext.cs
@@ -0,0 +1,14 @@
+﻿using CrudWebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudWebAPI.Data
+{
+    public class ContactsAPIDbContext : DbContext
+    {
+        public ContactsAPIDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
